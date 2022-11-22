e7b32178ed0f3d0877390232d548dc4a808a3317
muhammed anshif <mohdanshif22@gmail.com>
2022-11-22 20:14:21 +0530

    css commit

--- index/urls.py
+++ index/urls.py
@@ -5,14 +5,14 @@ from . import views
 urlpatterns=[
 
     path('',views.ind),
-    path('heartcentre/',views.heartcentre),
-    path('register/',views.register),
+    path('heartcentre/',views.heartcentre, name = "heartcentre"),
+    path('register/',views.register, name = "register"),
     path('login',views.login, name= 'log'),
-    path('book_appoinment/',views.book_appoinment),
-    path('booking_date/',views.booking_date),
-    path('about/',views.about),
-    path('cardiology/',views.cardiology),
-    path('enquiry/',views.enquiry),
+    path('book_appoinment/',views.book_appoinment, name = "book_appoinment"),
+    path('booking_date/',views.booking_date, name = "booking_date"),
+    path('about/',views.about, name = "about"),
+    path('cardiology/',views.cardiology, name = "cardiology"),
+    path('enquiry/',views.enquiry, name = "enquiry"),
 
 
 
